af816945c8caa86109731e086155cd8b349bf74e
ugesh <ugesh.roopchand@hotmail.com>
2018-08-09 15:44:25 +0400

    first commit

---
+++ BurgerBurger/BurgerBurger/Model/FoodListModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BurgerBurger.Pages
+{
+    class FoodListModel
+    {
+        public string FoodName { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ BurgerBurger/BurgerBurger/Pages/FoodListsPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BurgerBurger.Pages
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class FoodListsPage : ContentPage
+	{
+		public FoodListsPage ()
+		{
+			InitializeComponent ();
+
+            listView.ItemsSource = new List<FoodListModel>
+            {
+                new FoodListModel { FoodName ="Burger", ImageUrl="burger.png"},
+                new FoodListModel { FoodName ="Pizza", ImageUrl = "pizza.png"},
+                new FoodListModel { FoodName ="Sandwich", ImageUrl = "sandwich.png"},
+                new FoodListModel { FoodName ="Coffee", ImageUrl = "coffee.png"},
+                new FoodListModel { FoodName ="Beer", ImageUrl = "Beer.png"},
+                new FoodListModel { FoodName ="Desserts", ImageUrl = "Desserts.png"}
+
+            };
+		}
+	}
+}
